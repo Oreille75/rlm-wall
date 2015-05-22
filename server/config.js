442df0d8f544a522476ea4fbec056ee3551a1e31
Oreille75 <aurelienfouache@gmail.com>
2015-05-22 13:46:40 +0200

    conig

--- server/config.js
+++ server/config.js
@@ -1,44 +1,44 @@
-var base_url = 'http://rlm-wall.xxx.com';
+var base_url = 'https://rlm-wall.cleverapps.io';
 
 var twitter = {
-  consumer_key: 'xxxxxxxxxxxxxxxxxxxxxxxxx',
-  consumer_secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-  token: 'xxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-  token_secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+  consumer_key: 'OvaZRnqJ5xmdrpswpU6J1AFba',
+  consumer_secret: '9iE0SqWfmaUHXNsMwOOMV7dxEiBzZoxVSbkr3qRRK5DaLyAs5L',
+  token: '87213969-XzMckTpAKddabSh8tmv08nVd8nAeCgsue9DGh0cwG',
+  token_secret: '7qRppSf4zIZtLjKHzqSUqknYTyoZ0A5U7oX32ydz3c6to'
 };
 
 var instagram = {
-  client_id: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-  client_secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+  client_id: '75ccfa63082d43d7bae7178ae3f1d4ad',
+  client_secret: '5c91a8d198f748fcb97302a5585cfca5'
 };
 
-var mongodb = 'mongodb://localhost/RlmWall';
+var mongodb = 'b07idcxfob8ynqa.mongodb.clvrcld.net';
 
 if (process.env.NODE_ENV === 'home') {
-  base_url = 'http://xxx.xxx.com:8080';
+  base_url = 'https://rlmio.herokuapp.com/';
   twitter = {
-    consumer_key: 'xxxxxxxxxxxxxxxxxxxxxxxxx',
-    consumer_secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    token: 'xxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    token_secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    consumer_key: 'OvaZRnqJ5xmdrpswpU6J1AFba',
+    consumer_secret: '9iE0SqWfmaUHXNsMwOOMV7dxEiBzZoxVSbkr3qRRK5DaLyAs5L',
+    token: '87213969-XzMckTpAKddabSh8tmv08nVd8nAeCgsue9DGh0cwG',
+    token_secret: '7qRppSf4zIZtLjKHzqSUqknYTyoZ0A5U7oX32ydz3c6to'
   };
   instagram = {
-    client_id: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    client_secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    client_id: '75ccfa63082d43d7bae7178ae3f1d4ad',
+    client_secret: '5c91a8d198f748fcb97302a5585cfca5'
   };
 }
 
 if (process.env.NODE_ENV === 'test') {
   base_url = 'http://rlm-wall-preprod.xxx.com';
   twitter = {
-    consumer_key: 'xxxxxxxxxxxxxxxxxxxxxxxxx',
-    consumer_secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    token: 'xxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    token_secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    consumer_key: 'OvaZRnqJ5xmdrpswpU6J1AFba',
+    consumer_secret: '9iE0SqWfmaUHXNsMwOOMV7dxEiBzZoxVSbkr3qRRK5DaLyAs5L',
+    token: '87213969-XzMckTpAKddabSh8tmv08nVd8nAeCgsue9DGh0cwG',
+    token_secret: '7qRppSf4zIZtLjKHzqSUqknYTyoZ0A5U7oX32ydz3c6to'
   };
   instagram = {
-    client_id: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    client_secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    client_id: '75ccfa63082d43d7bae7178ae3f1d4ad',
+    client_secret: '5c91a8d198f748fcb97302a5585cfca5'
   };
   mongodb = 'mongodb://localhost/RlmWallTest';
 }
